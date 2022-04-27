487e52ceb081a4bd954aea72d5b0f74608c7f7a1
Herbew <herbew@gmail.com>
2022-04-27 20:19:21 +0700

    shout test

--- test/flowcontrol_test.exs
+++ test/flowcontrol_test.exs
@@ -5,8 +5,14 @@ defmodule BobFlowControlTest do
 		assert BobFlowControl.hey("   ") == "Fine. Be that way!"
 	end #test "1. Blank Phrase" do
 	
-	test "2. Question with char ? in latest." do
+	test "2. Question with char ? in the latest sentence." do
 		assert BobFlowControl.hey("Is it okay?") == "Sure."
 		assert BobFlowControl.hey("?") == "Sure."
-	end #test "2. Question with char ? in latest." do
+	end #test "2. Question with char ? in the latest sentence." do
+	
+	test "3. Is Shout phrase" do
+		assert BobFlowControl.hey("WARNING") == "Woah, chill out!"
+		assert BobFlowControl.hey("WANTED") == "Woah, chill out!"
+	end #test "3. Is Shout phrase" do
+	
 end
