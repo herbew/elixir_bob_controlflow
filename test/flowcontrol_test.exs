e532d245644e4d671ece00205e7f1d6492424bfb
Herbew <herbew@gmail.com>
2022-04-27 20:14:12 +0700

    question

--- test/flowcontrol_test.exs
+++ test/flowcontrol_test.exs
@@ -5,4 +5,8 @@ defmodule BobFlowControlTest do
 		assert BobFlowControl.hey("   ") == "Fine. Be that way!"
 	end #test "1. Blank Phrase" do
 	
+	test "2. Question with char ? in latest." do
+		assert BobFlowControl.hey("Is it okay?") == "Sure"
+		assert BobFlowControl.hey("?") == "Sure"
+	end #test "2. Question with char ? in latest." do
 end
