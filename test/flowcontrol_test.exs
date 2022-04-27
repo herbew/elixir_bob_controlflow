a7d835d3619d38572b816ca85160e938180995fc
Herbew <herbew@gmail.com>
2022-04-27 20:25:07 +0700

    test the others phrase

--- test/flowcontrol_test.exs
+++ test/flowcontrol_test.exs
@@ -5,14 +5,23 @@ defmodule BobFlowControlTest do
 		assert BobFlowControl.hey("   ") == "Fine. Be that way!"
 	end #test "1. Blank Phrase" do
 	
+	@tag :pending
 	test "2. Question with char ? in the latest sentence." do
 		assert BobFlowControl.hey("Is it okay?") == "Sure."
 		assert BobFlowControl.hey("?") == "Sure."
 	end #test "2. Question with char ? in the latest sentence." do
 	
+	@tag :pending
 	test "3. Is Shout phrase" do
 		assert BobFlowControl.hey("WARNING") == "Woah, chill out!"
 		assert BobFlowControl.hey("WANTED") == "Woah, chill out!"
 	end #test "3. Is Shout phrase" do
+	
+	@tag :pending
+	test "4. The others phrase" do
+		assert BobFlowControl.hey("Don't forget!") == "Whatever."
+		assert BobFlowControl.hey("Stay calm!") == "Whatever."
+		assert BobFlowControl.hey("Are you sure? with your statement..") == "Whatever."
+	end #test "4. The others phrase" do
 	
 end
